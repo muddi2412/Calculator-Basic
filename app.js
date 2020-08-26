4337450ea0bf763c35eb5e64af0f9daee7be0618
MUHAMMAD MUDASSIR <64653368+muddi2412@users.noreply.github.com>
2020-08-26 19:14:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ function getclear(){
     result.value = "";
 }
 
+function getback(){
+    var result = document.getElementById('result');
+    result.value = result.value.substr(0,result.value.length - 1);
+}
+
 function getresult(){
     var result = document.getElementById('result');
     result.value = eval(result.value);
